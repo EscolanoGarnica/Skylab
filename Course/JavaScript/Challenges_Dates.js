7a0f94a04277fd8c895c27e44295161d81aa10aa
Micheloncio <Michel@Seth-2.local>
2017-09-28 10:21:39 +0200

    Update Course/JavaScript/Challenges_Dates.js

--- Course/JavaScript/Challenges_Dates.js
+++ Course/JavaScript/Challenges_Dates.js
@@ -1,4 +1,29 @@
 /**
+*Dates
+*
+*Write a JavaScript function to check whether an input is a date object or not
+*/
+function is_date(input)
+{
+	if(input instanceof Date)
+	{
+		return true;
+	}else{
+		return false;
+	}
+}
+/**
+*Current Date
+*
+*Write a JavaScript function to get the current date Hint: Pass a separator as an argument.
+*/
+function currentDate(separator)
+{
+	var date =new Date();
+	return (date.getDate() + separator + (date.getMonth()+1) + separator + date.getFullYear());
+}
+
+/**
 *Get Days
 *
 *Write a JavaScript function to get the number of days in a month.
@@ -6,5 +31,6 @@
 
 function getDaysInMonth(month,year)
 {
-	
+	return new Date(year, month, 0).getDate();
 }
+
